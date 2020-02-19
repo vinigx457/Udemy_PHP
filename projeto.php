ea12233fcdea9ef70d2fd229a773796af66b6187
vinicius oliveira <vinigx457@gmail.com>
2020-02-19 00:55:54 -0300

    mudei o primeiro echo e adcionei o oola udemy

--- projeto.php
+++ projeto.php
@@ -1,7 +1,8 @@
 <?php //tag de abertura
 
-echo "Meu primeiro codigo - <strong>Ola mundo"; // echo faz com que apareça na tela - mesmo como  texto simples. Ele interpreta como um HTML
+echo "Meu primeiro codigo - <strong>Ola mundo 456"; // echo faz com que apareça na tela - mesmo como  texto simples. Ele interpreta como um HTML
 
+echo "Ola Udemy"
 ?> 
 
 
